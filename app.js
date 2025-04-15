6f53ee0a2d529d9b52f014ceaa72ba0ae683572c
Aryan Neupane <official.aryanneupane@gmail.com>
2025-04-15 16:37:28 +0545

    dom learning basics

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
- 
+ let a =document.querySelector('h1')
+ a.innerHTML="Changed Html"
+a.style.color="aqua"
+a.style.backgroundColor="orange"
+
+a.addEventListener("click",()=>{
+     a.innerHTML="Changed Html"
+a.style.color="aqua"
+a.style.backgroundColor="orange"
+console.log("Kxa ho halkhabar")
+})
+
+
+let btn = document.querySelector('.btn');
+let bulb = document.querySelector('.bulb');
+let flag=0
+btn.addEventListener("click", () => {
+   if(flag==0){
+    bulb.style.backgroundColor = 'white';
+    flag+=1;
+   }else{
+    bulb.style.backgroundColor = 'black';
+    flag-=1;
+   }
+});
+
+
